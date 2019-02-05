5f14289f099021f866cff8e7f7235cd4fbe7dca2
dave woodall <github@davewoodall.com>
2019-02-05 13:23:13 -0500

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="section">
+        <GitHub msg="hello, Brightwheel!" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import GitHub from "./components/GitHub.vue";
+
+export default {
+  name: "app",
+  components: {
+    GitHub
+  }
+};
+</script>
+
+<style lang="scss">
+@import "bulma";
+</style>
---
+++ src/components/GitHub.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <h1 class="title">{{ msg }}</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String
+  }
+};
+</script>
